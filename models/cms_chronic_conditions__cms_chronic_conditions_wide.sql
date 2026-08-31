93b357c7496259fec912e899ac26ab5446e629bd
Aaron Neiderhiser <anneiderhiser@gmail.com>
2026-08-31 17:07:46 -0600

    Preserve source-scoped chronic condition logic

--- models/cms_chronic_conditions__cms_chronic_conditions_wide.sql
+++ models/cms_chronic_conditions__cms_chronic_conditions_wide.sql
@@ -11,6 +11,7 @@ conditions as (
 
     select
           chronic_conditions_unioned.person_id
+        , chronic_conditions_unioned.data_source
         , chronic_conditions.condition_column_name
         , 1 as condition_count
     from {{ ref('cms_chronic_conditions__cms_chronic_conditions_long') }} as chronic_conditions_unioned
@@ -21,8 +22,9 @@ conditions as (
 )
 
 
-   select
-    p.person_id
+select
+      p.person_id
+    , p.data_source
     , {{ dbt_utils.pivot(
           column='condition_column_name'
         , values=dbt_utils.get_column_values(ref ('cms_chronic_conditions__cms_chronic_conditions_hierarchy'), 'condition_column_name', order_by= 'condition_column_name')
@@ -31,7 +33,10 @@ conditions as (
         , else_value= 0
         , quote_identifiers = False
       ) }}
-from {{ var('patient') }} p
-left join conditions on p.person_id = conditions.person_id
+from {{ var('patient') }} as p
+left join conditions
+    on p.person_id = conditions.person_id
+    and p.data_source = conditions.data_source
 group by
-    p.person_id
+      p.person_id
+    , p.data_source
--- models/staging/cms_chronic_conditions__stg_cms_chronic_condition_all.sql
+++ models/staging/cms_chronic_conditions__stg_cms_chronic_condition_all.sql
@@ -4,93 +4,126 @@ with chronic_conditions as (
 
 ),
 
-patient_encounters as (
+encounters as (
+
+    select
+          person_id
+        , encounter_id
+        , encounter_start_date
+        , drg_code
+        , drg_code_type
+        , data_source
+    from {{ var('encounter') }}
+
+),
+
+patient_conditions as (
 
     select
           encounter.person_id
         , encounter.encounter_id
         , encounter.encounter_start_date
-        , encounter.drg_code as ms_drg_code
         , encounter.data_source
-        , replace(condition.normalized_code,'.','') as condition_code
+        , replace(condition.normalized_code, '.', '') as condition_code
         , condition.code_system as condition_code_type
-        , replace(procedure_records.normalized_code,'.','') as procedure_code
+    from encounters as encounter
+    inner join {{ var('condition') }} as condition
+        on encounter.encounter_id = condition.encounter_id
+        and encounter.data_source = condition.data_source
+
+),
+
+patient_procedures as (
+
+    select
+          encounter.person_id
+        , encounter.encounter_id
+        , encounter.encounter_start_date
+        , encounter.data_source
+        , replace(procedure_records.normalized_code, '.', '') as procedure_code
         , procedure_records.code_system as procedure_code_type
-    from {{ var('encounter') }} as encounter
-         left join {{ var('condition') }} as condition
-             on encounter.encounter_id = condition.encounter_id
-         left join {{ var('procedure') }} as procedure_records
-             on encounter.encounter_id = procedure_records.encounter_id
+    from encounters as encounter
+    inner join {{ var('procedure') }} as procedure_records
+        on encounter.encounter_id = procedure_records.encounter_id
+        and encounter.data_source = procedure_records.data_source
 
 ),
 
 inclusions_diagnosis as (
 
     select
-          patient_encounters.person_id
-        , patient_encounters.encounter_id
-        , patient_encounters.encounter_start_date
-        , patient_encounters.data_source
+          patient_conditions.person_id
+        , patient_conditions.encounter_id
+        , patient_conditions.encounter_start_date
+        , patient_conditions.data_source
         , chronic_conditions.chronic_condition_type
         , chronic_conditions.condition_category
         , chronic_conditions.condition
-    from patient_encounters
-         inner join chronic_conditions
-             on patient_encounters.condition_code = chronic_conditions.code
+    from patient_conditions
+    inner join chronic_conditions
+        on patient_conditions.condition_code = chronic_conditions.code
+        and lower(patient_conditions.condition_code_type) =
+            lower(chronic_conditions.code_system)
     where chronic_conditions.inclusion_type = 'Include'
-    and chronic_conditions.code_system = 'ICD-10-CM'
-    and chronic_conditions.additional_logic = 'None'
+      and lower(chronic_conditions.code_system) = 'icd-10-cm'
+      and chronic_conditions.additional_logic = 'None'
 
 ),
 
 inclusions_procedure as (
 
     select
-          patient_encounters.person_id
-        , patient_encounters.encounter_id
-        , patient_encounters.encounter_start_date
-        , patient_encounters.data_source
+          patient_procedures.person_id
+        , patient_procedures.encounter_id
+        , patient_procedures.encounter_start_date
+        , patient_procedures.data_source
         , chronic_conditions.chronic_condition_type
         , chronic_conditions.condition_category
         , chronic_conditions.condition
-    from patient_encounters
-         inner join chronic_conditions
-             on patient_encounters.procedure_code = chronic_conditions.code
+    from patient_procedures
+    inner join chronic_conditions
+        on patient_procedures.procedure_code = chronic_conditions.code
+        and lower(patient_procedures.procedure_code_type) =
+            lower(chronic_conditions.code_system)
     where chronic_conditions.inclusion_type = 'Include'
-    and chronic_conditions.code_system in ('ICD-10-PCS', 'HCPCS')
-    and chronic_conditions.additional_logic = 'None'
+      and lower(chronic_conditions.code_system) in ('icd-10-pcs', 'hcpcs')
+      and chronic_conditions.additional_logic = 'None'
 
 ),
 
 inclusions_ms_drg as (
 
     select
-          patient_encounters.person_id
-        , patient_encounters.encounter_id
-        , patient_encounters.encounter_start_date
-        , patient_encounters.data_source
+          encounters.person_id
+        , encounters.encounter_id
+        , encounters.encounter_start_date
+        , encounters.data_source
         , chronic_conditions.chronic_condition_type
         , chronic_conditions.condition_category
         , chronic_conditions.condition
-    from patient_encounters
-         inner join chronic_conditions
-             on patient_encounters.ms_drg_code = chronic_conditions.code
+    from encounters
+    inner join chronic_conditions
+        on encounters.drg_code = chronic_conditions.code
+        and lower(encounters.drg_code_type) = lower(chronic_conditions.code_system)
     where chronic_conditions.inclusion_type = 'Include'
-    and chronic_conditions.code_system = 'MS-DRG'
-    and chronic_conditions.additional_logic = 'None'
+      and lower(chronic_conditions.code_system) = 'ms-drg'
+      and chronic_conditions.additional_logic = 'None'
 
 ),
 
 exclusions_diagnosis as (
 
     select distinct
-          patient_encounters.encounter_id
+          patient_conditions.encounter_id
+        , patient_conditions.data_source
         , chronic_conditions.condition
-    from patient_encounters
-         inner join chronic_conditions
-             on patient_encounters.condition_code = chronic_conditions.code
+    from patient_conditions
+    inner join chronic_conditions
+        on patient_conditions.condition_code = chronic_conditions.code
+        and lower(patient_conditions.condition_code_type) =
+            lower(chronic_conditions.code_system)
     where chronic_conditions.inclusion_type = 'Exclude'
-    and chronic_conditions.code_system = 'ICD-10-CM'
+      and lower(chronic_conditions.code_system) = 'icd-10-cm'
 
 ),
 
@@ -124,7 +157,8 @@ select distinct
     , cast(inclusions_unioned.condition as {{ dbt.type_string() }}) as condition
     , cast(inclusions_unioned.data_source as {{ dbt.type_string() }}) as data_source
 from inclusions_unioned
-     left join exclusions_diagnosis
-         on inclusions_unioned.encounter_id = exclusions_diagnosis.encounter_id
-         and inclusions_unioned.condition = exclusions_diagnosis.condition
+left join exclusions_diagnosis
+    on inclusions_unioned.encounter_id = exclusions_diagnosis.encounter_id
+    and inclusions_unioned.data_source = exclusions_diagnosis.data_source
+    and inclusions_unioned.condition = exclusions_diagnosis.condition
 where exclusions_diagnosis.encounter_id is null
